c3d56ec53eea198547c565bf06572800f0f1f6b9
NofarVered <nofarss79@gmail.com>
2022-11-21 01:01:08 +0200

    server

--- backend/server.py
+++ backend/server.py
@@ -1,1 +1,33 @@
-print("hello world")
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import transactions_router, categories_router, users_router
+
+app = FastAPI()
+
+app.include_router(transactions_router.router)
+app.include_router(categories_router.router)
+app.include_router(users_router.router)
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def root():
+    return "server is running"
+
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
